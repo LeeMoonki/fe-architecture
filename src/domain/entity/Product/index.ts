f5d311acbff1ab8af4ee991a234d94714a522a5d
Moonki Lee <shinbaek89@gmail.com>
2022-03-02 00:42:54 +0900

    상품 엔티티 정의

---
+++ src/domain/entity/Product/index.ts
@@ -0,0 +1,53 @@
+class Product {
+  private _id: number;
+
+  private _name: string;
+
+  private _price: number;
+
+  private _discountAmount: number;
+
+  private _registerDate: string;
+
+  constructor({
+    id,
+    name,
+    price,
+    discountAmount,
+    registerDate,
+  }: {
+    id: number;
+    name: string;
+    price: number;
+    discountAmount: number;
+    registerDate: string;
+  }) {
+    this._id = id;
+    this._name = name;
+    this._price = price;
+    this._discountAmount = discountAmount;
+    this._registerDate = registerDate;
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get price() {
+    return this._price;
+  }
+
+  get discountAmount() {
+    return this._discountAmount;
+  }
+
+  get registerDate() {
+    return this._registerDate;
+  }
+}
+
+export default Product;
